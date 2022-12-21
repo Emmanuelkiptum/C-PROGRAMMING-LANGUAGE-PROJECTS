945a1fa09176024541987839b500e68d9e7f3168
Emmanuelkiptum <91413986+Emmanuelkiptum@users.noreply.github.com>
2022-12-21 16:11:54 +0300

    Add files via upload

---
+++ 2. AREA OF A RECTANGLE.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int length,width;
+int area;
+printf("Enter the length:\n");
+scanf("%d",&length);
+printf("Enter the width:\n");
+scanf("%d",&width);
+area=length*width;
+printf("the area is %d",area);
+return 0;
+}
