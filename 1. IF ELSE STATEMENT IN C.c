60fd656639fe70f817277a4ffc8998fd00768958
Emmanuelkiptum <91413986+Emmanuelkiptum@users.noreply.github.com>
2022-12-21 16:11:33 +0300

    Add files via upload

---
+++ 1. IF ELSE STATEMENT IN C.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+   int a,b;
+   printf("Enter the value of a:");
+   scanf("%d",&a);
+   printf("\nEnter the value of b:");
+   scanf("%d",&b);
+   if(a>b){
+   	printf("\nthe value is correct:");
+   }
+   else{
+   	    	printf("\nthe value is incorret pin:");
+   }
+	return 0;
+}
